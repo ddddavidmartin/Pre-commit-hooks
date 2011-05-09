ac2f62f48b552b4c5ddba40e5a41cc1b9dac4e1b
David Martin <david.martin.mailbox@googlemail.com>
2011-05-09 21:39:56 +0200

    Make sure that all hooks are executable in install_hooks.sh.
    Print OK if they are executable or a hint on how to make them executable
    with chmod in case they are not.

--- install_hooks.sh
+++ install_hooks.sh
@@ -25,19 +25,34 @@ copy_hooks() {
     # create hooks subfolder if it does not yet exist
     mkdir -p $1/hooks
 
+    echo "Copying hooks to destination directory:"
     for hook in $HOOKS
     do
         echo "Copying $hook to $1/hooks."
         cp -i $SCRIPTPATH/$hook $1/hooks
     done
+
+    echo ""
+    echo "Checking if hooks are executable:"
+    for hook in $HOOKS
+    do
+        if [ ! -x $1/hooks/$hook ] ; then
+            echo "$hook is not executable. Fix it by calling"
+            echo "sudo chmod +x $1/hooks/$hook"
+        else
+            echo "$hook OK."
+        fi
+    done
 }
 
 
+echo ""
 echo "Git pre-commit hook installation."
 if [ $# = 1 ] ; then
     if [ -d "$1/.git" ] ; then
         copy_hooks $1/.git
-        echo "Make sure all copied files are executable."
+        echo ""
+        echo "Finished installation."
     else
         echo "Error: $1/.git does not exist."
         echo "Are you sure $1 is the root directory of your local git repository?"
